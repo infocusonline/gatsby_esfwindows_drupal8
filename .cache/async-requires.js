a6f1ea7a286efc43f861cff8c67c466398a46ad4
Miguel Tejeda <tejedamiguel6@aol.com>
2020-07-23 14:47:51 -0400

    testing work git

---
+++ .cache/async-requires.js
@@ -0,0 +1,22 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-accessories-js": () => import("./../src/pages/accessories.js" /* webpackChunkName: "component---src-pages-accessories-js" */),
+  "component---src-pages-blog-js": () => import("./../src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
+  "component---src-pages-contact-us-js": () => import("./../src/pages/contact-us.js" /* webpackChunkName: "component---src-pages-contact-us-js" */),
+  "component---src-pages-custom-js": () => import("./../src/pages/custom.js" /* webpackChunkName: "component---src-pages-custom-js" */),
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-materials-js": () => import("./../src/pages/materials.js" /* webpackChunkName: "component---src-pages-materials-js" */),
+  "component---src-pages-our-company-js": () => import("./../src/pages/our-company.js" /* webpackChunkName: "component---src-pages-our-company-js" */),
+  "component---src-pages-products-js": () => import("./../src/pages/products.js" /* webpackChunkName: "component---src-pages-products-js" */),
+  "component---src-pages-quote-received-js": () => import("./../src/pages/quote-received.js" /* webpackChunkName: "component---src-pages-quote-received-js" */),
+  "component---src-templates-accessories-js": () => import("./../src/templates/accessories.js" /* webpackChunkName: "component---src-templates-accessories-js" */),
+  "component---src-templates-blog-js": () => import("./../src/templates/blog.js" /* webpackChunkName: "component---src-templates-blog-js" */),
+  "component---src-templates-custom-js": () => import("./../src/templates/custom.js" /* webpackChunkName: "component---src-templates-custom-js" */),
+  "component---src-templates-materials-js": () => import("./../src/templates/materials.js" /* webpackChunkName: "component---src-templates-materials-js" */),
+  "component---src-templates-products-js": () => import("./../src/templates/products.js" /* webpackChunkName: "component---src-templates-products-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,25 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/404.js"))),
+  "component---src-pages-accessories-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/accessories.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/blog.js"))),
+  "component---src-pages-contact-us-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/contact-us.js"))),
+  "component---src-pages-custom-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/custom.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/index.js"))),
+  "component---src-pages-materials-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/materials.js"))),
+  "component---src-pages-our-company-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/our-company.js"))),
+  "component---src-pages-products-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/products.js"))),
+  "component---src-pages-quote-received-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/pages/quote-received.js"))),
+  "component---src-templates-accessories-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/templates/accessories.js"))),
+  "component---src-templates-blog-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/templates/blog.js"))),
+  "component---src-templates-custom-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/templates/custom.js"))),
+  "component---src-templates-materials-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/templates/materials.js"))),
+  "component---src-templates-products-js": hot(preferDefault(require("/Users/migueltejeda/Desktop/esfwindows:d8:gatsby/src/templates/products.js")))
+}
+
