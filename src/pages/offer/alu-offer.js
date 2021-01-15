7da04cb54661bfac2aa2a915797573256eded61d
Miguel Tejeda <tejedamiguel6@aol.com>
2021-01-15 09:22:57 -0500

    debugging

---
+++ src/pages/offer/alu-offer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const AluOffer = () => {
+  return (
+    <Layout>
+      <h1>all of items here</h1>
+    </Layout>
+  )
+}
+
+export default AluOffer
