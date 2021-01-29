0bf4735efca40a203f66263227acded435219a5e
tejedamiguel6 <tejedamiguel6@aol.com>
2021-01-29 09:03:25 -0500

    lol went back to the old form

--- src/pages/offer/pvc-offer/easyslide.js
+++ src/pages/offer/pvc-offer/easyslide.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../../components/Layout'
-import FormOptionTwo from '../../../components/forms/FormOptionTwo'
+import Form from '../../../components/forms/Form'
 
 const EasySlide = () => {
   return (
     <Layout>
-      <div style={{ marginTop: '90px' }}></div>
+      <div style={{ marginTop: '90px' }}>
+        <Form />
+      </div>
     </Layout>
   )
 }
--- src/pages/offer/pvc-offer/psk.js
+++ src/pages/offer/pvc-offer/psk.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../../../components/Layout'
+import Form from '../../../components/forms/Form'
 
 const Psk = () => {
   return (
     <Layout>
-      <div style={{ marginTop: '90px' }}></div>
+      <div style={{ marginTop: '90px' }}>
+        <Form />
+      </div>
     </Layout>
   )
 }
