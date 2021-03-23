a1172406a8d31d52c4516118e4bb04a75eeb9c47
tejedamiguel6 <tejedamiguel6@aol.com>
2021-03-23 15:03:37 -0400

    another page

--- src/pages/products/aluron-wood.js
+++ src/pages/products/aluron-wood.js
@@ -1,14 +1,90 @@
 import React from 'react'
+import { useStaticQuery, Link, graphql } from 'gatsby'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
 import Layout from '../../components/Layout'
 
 const AluronWood = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allNodeAlucladWoodSubItems {
+        edges {
+          node {
+            id
+            title
+            relationships {
+              field_aluronwood_subitems {
+                localFile {
+                  childImageSharp {
+                    fixed(height: 360, width: 360) {
+                      ...GatsbyImageSharpFixed
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(data, '@@@@@!')
   return (
     <div>
       <Layout>
-        <h1>aluron wood stuff here</h1>
+        <FlexContainer>
+          {data.allNodeAlucladWoodSubItems.edges.map(edge => {
+            const images =
+              edge.node.relationships.field_aluronwood_subitems[0]?.localFile
+                ?.childImageSharp.fixed
+            return (
+              <li>
+                <Link to="/pagenot">
+                  <SetImg fixed={images} />
+                  <h2>{edge.node.title}</h2>
+                </Link>
+              </li>
+            )
+          })}
+        </FlexContainer>
       </Layout>
     </div>
   )
 }
 
+const FlexContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  /* flex-direction: row-reverse; */
+  justify-content: center;
+  padding: 0.6rem;
+  margin: 90px;
+
+  li {
+    list-style-type: none;
+    padding: 10px;
+  }
+
+  h2 {
+    color: #2d385b;
+    font-size: 20px;
+    text-align: center;
+    margin-top: 15px;
+    text-align: left;
+    padding-bottom: 15px;
+    border-bottom: solid 1px blue;
+    width: 190px;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000000;
+  }
+`
+
+const SetImg = styled(Img)`
+  display: block !important;
+  width: 100%;
+  height: auto;
+`
 export default AluronWood
