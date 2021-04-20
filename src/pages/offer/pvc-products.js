22670156c7f939afd1b9b0635e20b3a992682536
tejedamiguel6 <tejedamiguel6@aol.com>
2021-04-20 13:42:56 -0400

    added bio

--- src/pages/offer/pvc-products.js
+++ src/pages/offer/pvc-products.js
@@ -2,12 +2,28 @@ import React from 'react'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
-
 import Layout from '../../components/Layout'
 
 const PvcOffer = () => {
   const data = useStaticQuery(graphql`
     query {
+      nodeOfferType(id: { eq: "085f47b6-a58f-5fb4-89b9-19dbdbd71e01" }) {
+        title
+        body {
+          value
+        }
+        relationships {
+          field_offer_type_image {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 1080, maxHeight: 450) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
       allNodePvcOfferSubItems {
         edges {
           node {
@@ -31,8 +47,21 @@ const PvcOffer = () => {
       }
     }
   `)
+  const about = data.nodeOfferType.body.value
+  const heroImage =
+    data.nodeOfferType.relationships.field_offer_type_image[0]?.localFile
+      .childImageSharp.fluid
   return (
     <Layout>
+      <Container>
+        <h1>{data.nodeOfferType.title}</h1>
+        {heroImage ? (
+          <div>
+            <ContainerImg fluid={heroImage} />
+          </div>
+        ) : null}
+      </Container>
+      <Bio dangerouslySetInnerHTML={{ __html: about }}></Bio>
       <FlexContainer>
         {data.allNodePvcOfferSubItems.edges.map(edge => {
           const images =
@@ -51,6 +80,35 @@ const PvcOffer = () => {
     </Layout>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 90px;
+  /* height: 500px; */
+  margin-bottom: 80px;
+
+  h1 {
+    margin-top: 90px;
+    padding-left: 30px;
+    margin-left: 20px;
+    font-size: 50px;
+  }
+`
+const ContainerImg = styled(Img)`
+  margin-left: auto;
+  order: 2;
+  width: 980px;
+  height: 21vw;
+  clip-path: polygon(10vw 0, 100% 0, 100% 100%, 0% 100%);
+`
+
+const Bio = styled.div`
+  margin: 0 auto;
+  max-width: 1080px;
+  font-size: 1.1rem;
+  line-height: 1.8rem;
+  text-align: center;
+`
 
 const FlexContainer = styled.ul`
   display: flex;
